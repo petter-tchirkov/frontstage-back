e50448aa1096db81929e877c9bda19aa2b0b7074
pettertchirkov <tchirkov.petter@gmail.com>
2021-12-29 16:03:34 +0200

    authorization

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = express( );
 const authRouter = require('./authRouter')
 
+
 app.use(express.json());
 app.use('/auth', authRouter)
 const start = async () => {
